5991f66779c51b2eb86e1229662906f92c9e6a6f
praveen-stox <praveen.matlab@gmail.com>
2021-07-20 14:03:11 +0530

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView,DetailView
+
+class BlogListView(ListView):
+    model=Post
+    template_name='home.html'
+
+class BlogDetailView(DetailView):
+    model=Post
+    template_name='post_detail.html'
+    
+        
+
+# Create your views here.
